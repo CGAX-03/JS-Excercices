8d267e86c079dd98c215b6cab0c2b54fc20c8713
Christian Gaxiola <101108044+CGAX-03@users.noreply.github.com>
2023-05-20 15:18:00 -0300

    Add files via upload

---
+++ Ex05.js
@@ -0,0 +1,18 @@
+//Verify if names are the same
+
+const name01 = "Chris"
+const name02 = "Gerald"
+const name03 = "Chris"
+
+if (name01 == name02) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+
+if (name01 == name03) {
+    console.log(true)
+} else {
+    console.log(false)
+}

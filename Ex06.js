fccb695e092df5156622f7fd2c511666ae250c7b
Christian Gaxiola <101108044+CGAX-03@users.noreply.github.com>
2023-05-20 15:18:10 -0300

    Add files via upload

---
+++ Ex06.js
@@ -0,0 +1,17 @@
+//Same as Ex05 but inverted
+const name01 = "Chris"
+const name02 = "Gerald"
+const name03 = "Chris"
+
+if (name01 == name02) {
+    console.log(false)
+} else {
+    console.log(true)
+}
+
+
+if (name01 == name03) {
+    console.log(false)
+} else {
+    console.log(true)
+}
---
+++ Ex07.js
@@ -0,0 +1,22 @@
+const name01 = "Chris"
+const name02 = "Gerald"
+const name03 = "Chris"
+
+const nameList = {name01, name02, name03}
+
+console.log(nameList)
+
+console.log("Are the names 01 and 02 the same?")
+if (name01 == name02) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+console.log("Are the names 01 and 03 the same?")
+if (name01 == name03) {
+    
+    console.log(true)
+} else {
+    console.log(false)
+}

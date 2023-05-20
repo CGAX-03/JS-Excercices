5d696927e94ca25ba1b0bfe074375c9bcc7663da
Christian Gaxiola <101108044+CGAX-03@users.noreply.github.com>
2023-05-20 15:19:41 -0300

    Add files via upload

---
+++ Ex10.js
@@ -0,0 +1,8 @@
+const num01 = 7
+const num02 = 3
+let rest = 0
+
+rest = (num01%num02)
+console.log(rest)
+
+//rest = (7%3)
---
+++ Ex11.js
@@ -0,0 +1,10 @@
+const num01 = 1
+const num02 = 2
+let res = 0
+
+if (num01 > num02) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+

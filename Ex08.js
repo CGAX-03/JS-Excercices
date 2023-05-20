c6542a17d946154ea210fcdf35bc7528265085e0
Christian Gaxiola <101108044+CGAX-03@users.noreply.github.com>
2023-05-20 15:18:21 -0300

    Add files via upload

---
+++ Ex08.js
@@ -0,0 +1,27 @@
+const people = {
+ chris:{
+    name: "Chris",
+    age: 20,
+    height: 1.80,
+},
+
+ cris: {
+    name: "Chris",
+    age: 20,
+    height: 1.80,
+},
+
+ cis: {
+    name: "Chris",
+    age: 20,
+    height: 1.80,
+},
+
+ cs: {
+    name: "Chris",
+    age: 20,
+    height: 1.80,
+}
+}
+
+console.log(people)

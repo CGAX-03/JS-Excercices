103b266217e6930aeef37d883833fc327343e376
Christian Gaxiola <101108044+CGAX-03@users.noreply.github.com>
2023-05-20 15:17:03 -0300

    Add files via upload

---
+++ Ex01.js
@@ -0,0 +1,8 @@
+//Sum 2 numbers
+
+const num01 = 1
+const num02 = 2
+let sum = num01 + num02
+
+console.log(sum)
+
---
+++ Ex02.js
@@ -0,0 +1,7 @@
+//Multiply 2 numbers
+
+const num01 = 2
+const num02 = 2
+let mult = num01 * num02
+
+console.log(mult)

2d20c9ada1072a898c8e5be6e24851b9d851a2dd
Christian Gaxiola <101108044+CGAX-03@users.noreply.github.com>
2023-05-20 15:17:48 -0300

    Add files via upload

---
+++ Ex04.js
@@ -0,0 +1,6 @@
+//"Hi! my name is (name 01) and my DevClub partner is (name 02)"
+
+const name01 = "Chris"
+const name02 = "Gerald"
+
+console.log("Hi! my name is "+ name01 + " and my DevClub partner is " + name02)

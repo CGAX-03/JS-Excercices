a21fac3910395b811d635d13fb049189ddd37a82
Christian Gaxiola <101108044+CGAX-03@users.noreply.github.com>
2023-05-21 21:29:42 -0300

    Add files via upload

---
+++ Ex12.js
@@ -0,0 +1,11 @@
+//Compare a number to 10
+
+let num = 11
+
+if (num > 10) {
+    console.log("Higher!!!")
+} else if (num ==  10){
+    console.log("Same!!!")
+} else {
+    console.log("Lower!!!")
+}

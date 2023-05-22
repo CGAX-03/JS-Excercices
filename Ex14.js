6f5514a1c97e8eafb2a9979da1d781b4fa357c6e
Christian Gaxiola <101108044+CGAX-03@users.noreply.github.com>
2023-05-21 21:30:02 -0300

    Add files via upload

---
+++ Ex14.js
@@ -0,0 +1,8 @@
+let num = (Math.ceil(Math.random() * 10))
+let win = (Math.ceil(Math.random() * 10))
+
+if (num == win) {
+    console.log("Congrats!, You've won")
+} else {
+    console.log("Ups... You've Lost")
+}
